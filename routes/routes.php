01a0283500f1e14465ee56ced9036bad4f1ebf75
dejongtiemen <92309586+dejongtiemen@users.noreply.github.com>
2023-03-14 07:59:44 +0100

    first change

---
+++ routes/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+/** @var Bramus\Router\Router $router */
+
+// Define routes here
+$router->get('/test', App\Controllers\IndexController::class . '@test');
+$router->get('/', App\Controllers\IndexController::class . '@test');
+$router->get('/facility', App\Controllers\IndexController::class . '@facility');
